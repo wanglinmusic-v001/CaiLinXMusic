3f8949aaa88fb19b36a6fac81248a461e11fed91
wanglinmusic-v001 <wanglin-music-v01@thepiratebay.cloud>
2025-02-04 11:42:51 +0630

    Update settings.py

--- CaiLinXMusic/plugins/bot/settings.py
+++ CaiLinXMusic/plugins/bot/settings.py
@@ -56,7 +56,7 @@ async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
         InputMediaVideo("https://files.catbox.moe/4ulx7l.mp4"),
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
+            [[InlineKeyboardButton(text=" နောက်သို့ ", callback_data=f"settingsback_helper")]]
         ),
     )
 
