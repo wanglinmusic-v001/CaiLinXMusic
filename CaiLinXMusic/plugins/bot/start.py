3cf9e0984d3532f58727af96842f1bab58c7cbe1
wanglinmusic-v001 <wanglin-music-v01@thepiratebay.cloud>
2025-02-04 10:33:07 +0630

    Update start.py

--- CaiLinXMusic/plugins/bot/start.py
+++ CaiLinXMusic/plugins/bot/start.py
@@ -98,42 +98,6 @@ async def start_pm(client, message: Message, _):
                 chat_id=config.LOGGER_ID,
                 text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
             )
-            else:
-
-        try:
-            out = private_panel(_)
-            may = await message.reply_text(f"**ᴅιиg ᴅσиg ꨄ︎❣️.....**")
-            await may.edit_text(f"**ᴅιиg ᴅσиg ꨄ︎.❣️....**")
-            await may.edit_text(f"**ᴅιиg ᴅσиg ꨄ︎..❣️...**")
-            await may.edit_text(f"**ᴅιиg ᴅσиg ꨄ︎...❣️..**")
-            await may.edit_text(f"**ᴅιиg ᴅσиg ꨄ︎....❣️.**")
-            await may.edit_text(f"**ᴅιиg ᴅσиg ꨄ︎.....❣️**")
-
-            await may.delete()
-            mays = await message.reply_text("**⚡ѕ**")
-            await asyncio.sleep(0.1)
-            await mays.edit_text("**⚡ѕт**")
-            # await asyncio.sleep(0.1)
-            await mays.edit_text("**⚡ѕтα**")
-            #  await asyncio.sleep(0.1)
-            await mays.edit_text("**⚡ѕтαя**")
-            # await asyncio.sleep(0.1)
-            await mays.edit_text("**⚡ѕтαят**")
-            # await asyncio.sleep(0.1)
-            await mays.edit_text("**⚡ѕтαятι**")
-            # await asyncio.sleep(0.1)
-            await mays.edit_text("**⚡ѕтαятιи**")
-            # await asyncio.sleep(0.1)
-            await mays.edit_text("**⚡ѕтαятιиg**")
-            # await asyncio.sleep(0.1)
-            await mays.edit_text("**⚡ѕтαятιиg.**")
-            await asyncio.sleep(0.1)
-            await mays.edit_text("**⚡ѕтαятιиg....**")
-            await asyncio.sleep(0.1)
-            await mays.edit_text("**⚡ѕтαятιиg.**")
-            await asyncio.sleep(0.1)
-            await mays.edit_text("**⚡ѕтαятιиg....**")
-
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
